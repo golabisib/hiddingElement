30f1234e6c6d99e059d91dc3b9a9a864e3d7586b
Roozbeh Rigi Jangjo <silentroozbeh@gmail.com>
2024-03-29 23:02:21 +0330

    the element hidding Project is added

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+  $('#btn1').click(function () {
+    $('p:nth-child(2)').hide();
+  });
+
+  $('#btn2').click(function () {
+    $('span.my-span').hide();
+  });
+
+  $('#btn3').click(function () {
+    $("input[type='email']").hide();
+  });
+
+  $('#btn4').click(function () {
+    $('input:checked').hide();
+  });
+
+  $('#btn5').click(function () {
+    $('div#my-div>div>span').hide();
+  });
+});
